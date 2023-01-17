353271d7191d7e6c10751612406080ad99825628
md-ataur <mdataur15@gmail.com>
2023-01-18 01:18:52 +0600

    crud operation done with postgresql

---
+++ car-parking-backend-with-mongoDB/src/models/index.js
@@ -0,0 +1,2 @@
+module.exports.Vehicle = require('./Vehicle');
+module.exports.User = require('./User');
---
+++ car-parking-backend-with-postgresQL/src/utils/ErrorCodes.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+const errorCode = {
+  "0": "UnknownError",
+
+  "6000": "Vehicles Get Error",
+  "6001": "Vehicle Add Error",
+  "6002": "Single Vehicle Get Error",
+  "6003": "Vehicle Update Error",
+  "6004": "Vehicle Delete Error",
+}
+
+module.exports = errorCode;
